ce9c976ff29850393d428c02d6ef9299517a8977
Elena Zabelina <zabelina92@mail.ru>
2019-10-14 00:48:52 +0300

    Занятие #26

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -4,12 +4,14 @@ import random
 
 
 def test_del_project(app):
-    if len(app.project.get_project_list()) == 0:
+    username = app.config['webadmin']['username']
+    password = app.config['webadmin']['password']
+    if len(app.soap.get_list_projects(username, password)) == 0:
         app.project.create(Project(name="name123"))
-    old_projects = app.project.get_project_list()
+    old_projects = app.soap.get_list_projects(username, password)
     random_project = random.choice(old_projects)
     app.project.delete_project_by_name(random_project.name)
     assert len(old_projects) - 1 == app.project.count()
-    new_projects = app.project.get_project_list()
+    new_projects = app.soap.get_list_projects(username, password)
     old_projects.remove(random_project)
     assert old_projects == new_projects
